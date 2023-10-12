66165a74b6042f71c32b7f78696c2f3d776fc969
22P31A0418 <119720034+22P31A0418@users.noreply.github.com>
2023-10-12 14:59:28 +0530

    n=int(input()) num=list(map(int, input().split())) se=int(input()) print(se in num)

--- Element_search_in_an_Array.py
+++ Element_search_in_an_Array.py
@@ -1,9 +1,4 @@
 n=int(input())
 num=list(map(int, input().split()))
 se=int(input())
-is_found = False
-for i in num:
-    if i==se:
-        is_found = True
-        break
-print(is_found)
+print(se in num)
