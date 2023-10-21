685b19b23bff84ab24fab6fa0ed577d6e5a8398c
22P31A0418 <119720034+22P31A0418@users.noreply.github.com>
2023-10-21 19:52:16 +0530

    def prime(n:int)->int:     k=int(n**0.5)+1     for i in range(2,k):         if(n%i==0):             return 0     return 1 def prev_prime(n):     while(n):         if(prime(n)):             break         else:             n-=1     return n def next_prime(n):     while(n):         if(prime(n)):             break         else:             n+=1     return n n=int(input()) fn=prev_prime(n) bn=next_prime(n) if(n-fn>bn-n):     print(bn-n) else:     print(n-fn)

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,27 @@
+def prime(n:int)->int:
+    k=int(n**0.5)+1
+    for i in range(2,k):
+        if(n%i==0):
+            return 0
+    return 1
+def prev_prime(n):
+    while(n):
+        if(prime(n)):
+            break
+        else:
+            n-=1
+    return n
+def next_prime(n):
+    while(n):
+        if(prime(n)):
+            break
+        else:
+            n+=1
+    return n
+n=int(input())
+fn=prev_prime(n)
+bn=next_prime(n)
+if(n-fn>bn-n):
+    print(bn-n)
+else:
+    print(n-fn)
