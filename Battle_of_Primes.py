deef03b39e464e13c4d192a4c9e6a5ed42e4ed04
22P31A0418 <119720034+22P31A0418@users.noreply.github.com>
2023-10-22 10:42:10 +0530

    def prime(n:int)->int:     if(n==1):         return 0     k=int(n**0.5)+2     for i in range(2,k):         if(n%i==0):             return 0     return 1 def next_prime(n):     if(prime(n)==1):         return n     else:         return next_prime(n+1) a=int(input()) b=int(input()) n=next_prime(a+b+1) print(n-a-b)

---
+++ Battle_of_Primes.py
@@ -0,0 +1,17 @@
+def prime(n:int)->int:
+    if(n==1):
+        return 0
+    k=int(n**0.5)+2
+    for i in range(2,k):
+        if(n%i==0):
+            return 0
+    return 1
+def next_prime(n):
+    if(prime(n)==1):
+        return n
+    else:
+        return next_prime(n+1)
+a=int(input())
+b=int(input())
+n=next_prime(a+b+1)
+print(n-a-b)
